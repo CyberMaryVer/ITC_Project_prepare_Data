3f8752d1c1c876a0ec672b7237b1f6732aeb9e85
maria startseva <maria.s.startseva@gmail.com>
2020-11-04 16:11:04 +0200

    Other stackexchange websites added, all constants are moved at the beginning

--- scrap_so.py
+++ scrap_so.py
@@ -1,6 +1,18 @@
 import pandas as pd # Data processing, CSV file
 import requests # Getting Webpage content
 from bs4 import BeautifulSoup as bs # Scraping webpages
+import sys
+
+REQUIRED_NUM_OF_ARGS = 2
+CHUNK_SIZE = 300
+
+URL = 'https://stackoverflow.com/questions'
+URL_UBUNTU = 'https://askubuntu.com/questions'
+URL_MATH = 'https://math.stackexchange.com/questions'
+
+TAGS = ['web-scraping', 'python', 'numpy', 'pandas']
+TAGS_UBUNTU = ['command-line', 'networking', 'python', 'mysql']
+TAGS_MATH = ['calculus', 'matrices', 'probability', 'functions']
 
 def get_answer(q_url):
     """
@@ -25,9 +37,7 @@ def get_answer(q_url):
             pass
     return answers
 
-
-
-def get_faq(url, tag, n_questions=50):
+def get_faq(url, tag, n_questions=CHUNK_SIZE):
     """
     The function uses url and tag to find and collect the frequently asked questions
     and relevant answers from stackoverflow.com. The function returns
@@ -87,22 +97,6 @@ def test_scrap(f1=False, f2=True):
         TAG = 'python'
         print(get_faq(URL, TAG, 40).head(10))
 
-
-def main():
-    """Main function"""
-
-    URL = 'https://stackoverflow.com/questions'
-    TAG = 'web-scraping'
-
-    df0 = get_faq(URL, TAG, 50)
-    df0.to_csv('scrap_so.csv')
-    print(df0.head())
-
-if __name__ == '__main__':
-    # main()
-    test_scrap()
-
-
 def get_answer(q_url):
     """
     The function extracts answers by url of question
@@ -126,51 +120,6 @@ def get_answer(q_url):
             pass
     return answers
 
-
-
-def get_faq(url, tag, n_questions=50):
-    """
-    The function uses url and tag to find and collect the frequently asked questions
-    and relevant answers from stackoverflow.com. The function returns
-    dataframe with questions, summary, link and list of answers.
-
-    :param url: URL = 'https://stackoverflow.com/questions'
-    :param tag: string (!StackOverflow tags only!)
-    :param n_questions: integer
-    :return: pd.DataFrame
-    """
-    simple_list = [[]]
-    for n in range(int(n_questions / 50) + 1):
-        url0 = url + "/tagged/" + tag + "?tab=votes&page={}&pagesize=50".format(n + 1)
-        print(url0)
-
-        r = requests.get(url0)
-        soup = bs(r.text, "html.parser")
-
-        # Extracting questions and links
-        link_hrefs = soup.select("a.question-hyperlink")
-        questions = [l.text.strip() for l in link_hrefs]  # questions list
-        href_tags = [url[:-10] + l['href'] for l in link_hrefs]  # links list
-
-        # Extracting additional information for question (summary)
-        summary_divs = soup.select("div.excerpt")
-        summaries = [i.text.strip() for i in summary_divs]  # summaries list
-
-        # Creating a list with all the data
-        for i in range(1, len(questions) - 1):
-            print('Retrieving question #{} on page {}...'.format(i, n + 1))
-            try:
-                answer_lst = get_answer(href_tags[i])
-                simple_list.append([questions[i], summaries[i], href_tags[i], answer_lst])
-            except:
-                pass
-
-    # Putting all of them together in a dataframe
-    df = pd.DataFrame(simple_list, columns=['question', 'summary', 'link', 'answers'])
-    df = df.head(n_questions + 1)
-
-    return df
-
 def test_scrap(f1=False, f2=True):
     """
     The test function, used for displaying the result of web-scrapping on the screen.
@@ -192,10 +141,37 @@ def test_scrap(f1=False, f2=True):
 def main():
     """Main function"""
 
-    URL = 'https://stackoverflow.com/questions'
-    TAG = 'web-scraping'
+    for tag in TAGS:
+        filename = tag + '.csv'
+        df = get_faq(URL, tag)
+        df.to_csv(filename)
+        print(df.head())
 
-    df0 = get_faq(URL, TAG, 300)
+def command_line():
+    """
+    ------DISABLED-------
+    get user input - file name and which option --ubuntu or --math
+    scrapes the data and writes result into the csv file
+    """
+
+    if len(sys.argv) != REQUIRED_NUM_OF_ARGS:
+        print("usage: ./scrap_so.py {--ubuntu | --math} filepath")
+        sys.exit(1)
+
+    option = sys.argv[1]
+    url = URL
+    tag = TAGS
+    if option == "--ubuntu":
+        url = URL_UBUNTU
+        tag = TAGS_UBUNTU
+    elif option == "--math":
+        url = URL_MATH
+        tag = TAGS_MATH
+    else:
+        print("unknown option: " + option)
+        sys.exit(1)
+
+    df0 = get_faq(url, tag[0], 30)
     df0.to_csv('scrap_so.csv')
     print(df0.head())
 
