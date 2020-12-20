7e4de55bcb57fd296754d90192c80791c4509067
David Frankenberg <david.frankenberg@alumnos.usm.cl>
2020-12-20 14:07:23 +0200

    Fix the Nonetype error

--- db/connection.py
+++ db/connection.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import config
 
-engine = create_engine(config.DATABASE_URL + '/' + config.DATABASE_NAME + '?charset=utf8mb4_ai_ci', echo=config.ECHO)
+engine = create_engine(config.DATABASE_URL + '/' + config.DATABASE_NAME + '?charset=utf8mb4', echo=config.ECHO)
 Session = sessionmaker(bind=engine)
 session = Session()
